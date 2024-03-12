f1ea4d0ed0e2ee492af2edfd858f7b776bbf9aea
hlafaille <hlafaille@proton.me>
2024-03-12 12:35:44 -0400

    #4 building lock

--- src/backend/lock.rs
+++ src/backend/lock.rs
@@ -1,6 +1,23 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct LockFile {
-    pub 
+    pub dependencies: Vec<Dependency>,
+}
+
+/**
+ * Represents a dependency in '.espresso/dependencies'.
+ */
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Dependency {
+    pub name: String,
+    pub fs_name: String,
+    pub checksum: String,
+}
+
+/**
+ * Get '.espresso'
+ */
+pub fn get_lock_file_from_fs() -> LockFile {
+
 }
