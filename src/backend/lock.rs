dcadae6c6c25eb2c6ba55418f6082a49957067dc
hlafaille <hlafaille@proton.me>
2024-03-13 12:29:52 -0400

    #4 working on state lockfile

--- src/backend/lock.rs
+++ src/backend/lock.rs
@@ -1,7 +1,11 @@
+use std::{error, fs, result};
+
 use serde::{Deserialize, Serialize};
 
+use super::context::{AbsoltuePaths, DynamicAbsolutePaths};
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct LockFile {
+pub struct StateLockFile {
     pub dependencies: Vec<Dependency>,
 }
 
@@ -15,9 +19,26 @@ pub struct Dependency {
     pub checksum: String,
 }
 
-/**
- * Get '.espresso'
- */
-pub fn get_lock_file_from_fs() -> LockFile {
+/// Get the `.espresso/state.lock` file from the filesystem.
+/// 
+/// # Arguments
+/// * `ap`: Reference to an `AbsolutePaths` struct
+/// 
+/// # Returns
+/// Returns a `LockFile` struct on success, propagating any errors in the process
+pub fn get_state_lockfile_from_fs(ap: &AbsoltuePaths) -> result::Result<StateLockFile, Box<dyn error::Error>> {
+    let contents = fs::read_to_string(&ap.state_lockfile)?;
+    let x: StateLockFile = toml::from_str(&contents)?;
+    Ok(x)
+}
 
+/// Initialize a new state lockfile
+pub fn initialize_state_lockfile(ap: &AbsoltuePaths) -> result::Result<(), Box<dyn error::Error>> {
+    let base = StateLockFile {
+        dependencies: vec![]
+    };
+
+    let toml_string = toml::to_string(&base)?;
+    fs::write(ap.state_lockfile.clone(), toml_string)?;
+    Ok(())
 }
