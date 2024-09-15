5dcb50f8b9884cd95ccecfceee4e19074bf3de3c
Hunter LaFaille <hlafaille@proton.me>
2024-09-15 02:45:40 +0000

    Removing accidentally committed files

--- ESPRESSO_DEBUG/java/xyz/hlafaille/espresso/Main.java
+++
@@ -1,8 +0,0 @@
-package xyz.hlafaille.espresso;
-import java.lang.System;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello from Espresso!");
-    }
-}
