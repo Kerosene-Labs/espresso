c31733d06442601d9d8f87948b6d416f6d83eebd
Hunter LaFaille <hlafaille@proton.me>
2024-09-21 03:20:24 +0000

    Refactoring

--- core/project/config.go
+++ core/project/config.go
@@ -167,3 +167,19 @@ func PersistConfig(cfg *ProjectConfig) error {
 
 	return nil
 }
+
+// GetConfigPath gets the absolute path to the config file
+func GetConfigPath() (*string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	if util.IsDebugMode() {
+		path := wd + "/ESPRESSO_DEBUG" + "/espresso.yml"
+		return &path, nil
+	} else {
+		path := wd + "/espresso.yml"
+		return &path, nil
+	}
+}
